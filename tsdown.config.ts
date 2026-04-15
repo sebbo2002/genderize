1db6c9576a82921fa6d731048adf70abfc0e3f38
Sebastian Pekarek <mail@sebbo.net>
2026-04-15 18:42:44 +0200

    chore: Merge branch 'develop' into template-updater/update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
     dts: true,
-    entry: ['src/lib/index.ts', 'src/bin/cli.ts', 'src/bin/start.ts'],
-    entry: ['src/lib/index.ts', 'src/bin/cli.ts', 'src/bin/start.ts'],
+    entry: ['src/lib/index.ts'],
     format: ['esm'],
     format: ['esm', 'cjs'],
     minify: true,
